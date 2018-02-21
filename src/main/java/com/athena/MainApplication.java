a7e42023e6e8ab8516dfddbf54af5ad5e2ff30fd
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-02-22 01:41:42 +0200

    Dependencies, Project Properties and Entity model creation for testing purposes
    Dependencies for PostGreSQL and annotations for entity mapping.
    
    Project Properties for PostGreSQL connectivity.
    
    Creation of model Entity "FoodCollection" for testing purpose, there is an example in main.

--- src/main/java/com/athena/MainApplication.java
+++ src/main/java/com/athena/MainApplication.java
@@ -1,13 +1,28 @@
 package com.athena;
 
+import com.athena.model.entities.FoodCollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MainApplication
 {
     public static void main(String[] args)
     {
+        List<FoodCollection> foodCollectionList = new ArrayList<FoodCollection>();
+
+        for(int i=1;i<=10;i++){
+            FoodCollection food = new FoodCollection(i,"food"+i,"food description"+i, 1.0*i);
+            foodCollectionList.add(food);
+        }
+
+        for(FoodCollection food : foodCollectionList){
+            System.out.println(food.getName()+", "+food.getDescription()+", "+food.getPrice()+", "+food.getFoodId());
+        }
+
         SpringApplication.run(MainApplication.class, args);
 
     }
---
+++ src/main/java/com/athena/model/entities/FoodCollection.java
@@ -0,0 +1,60 @@
+package com.athena.model.entities;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="food_collection")
+//@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
+public class FoodCollection implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int foodId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Double price;
+
+    public FoodCollection(int foodId, String name, String description, Double price) {
+        this.foodId = foodId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getFoodId() {
+        return foodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
