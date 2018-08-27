7c13cfb1d7cdf3dd057eec4e80869f125968cfbf
tony <giant_27@yahoo.gr>
2018-08-27 16:18:47 +0300

    Fixed data parser

--- src/main/java/com/athena/dao/CategoryDAO.java
+++ src/main/java/com/athena/dao/CategoryDAO.java
@@ -4,8 +4,10 @@ import com.athena.entities.Category;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.reflect.Type;
@@ -24,7 +26,12 @@ public class CategoryDAO
         Type REVIEW_TYPE = new TypeToken<List<Category>>() {}.getType();
 
         Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader("D:\\Documents\\data.json"));
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        //TODO: Null Pointer exception needs handling
+        File file = new File(classLoader.getResource("static/data/menu.json").getFile());
+
+        JsonReader reader = new JsonReader(new FileReader(file.getPath()));
         List<Category> allCategories = gson.fromJson(reader, new TypeToken<List<Category>>(){}.getType());
 
         return allCategories;
