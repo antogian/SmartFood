f0a08111d1697d762774f71234a1d82eb28a5373
tony <giant_27@yahoo.gr>
2018-12-16 21:57:59 +0200

    Changed Data Access Object

--- src/main/java/com/athena/dao/CategoryDAO.java
+++ src/main/java/com/athena/dao/CategoryDAO.java
@@ -16,6 +16,7 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,49 +33,49 @@ public class CategoryDAO
 
     public List<Category> getAllCategories() throws FileNotFoundException
     {
-//        Type REVIEW_TYPE = new TypeToken<List<Category>>() {}.getType();
-//
-//        Gson gson = new Gson();
-//
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        //TODO: Null Pointer exception needs handling
-//        File file = new File(classLoader.getResource("data/data.json").getFile());
-//
-//        JsonReader reader = new JsonReader(new FileReader(file.getPath()));
-//        List<Category> allCategories = gson.fromJson(reader, new TypeToken<List<Category>>(){}.getType());
-//
-//        return allCategories;
-        List<Category> allCats = getMongoCategories();
-        if(!allCats.isEmpty())
-            return allCats;
+        Type REVIEW_TYPE = new TypeToken<List<Category>>() {}.getType();
 
         Gson gson = new Gson();
+
         ClassLoader classLoader = getClass().getClassLoader();
-        InputStream in = classLoader.getResourceAsStream("data/data.json");
-        JsonReader reader = new JsonReader(new InputStreamReader(in));
+        //TODO: Null Pointer exception needs handling
+        File file = new File(classLoader.getResource("data/data.json").getFile());
+
+        JsonReader reader = new JsonReader(new FileReader(file.getPath()));
         List<Category> allCategories = gson.fromJson(reader, new TypeToken<List<Category>>(){}.getType());
 
         return allCategories;
+//        List<Category> allCats = getMongoCategories();
+//        if(!allCats.isEmpty())
+//            return allCats;
+//
+//        Gson gson = new Gson();
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        InputStream in = classLoader.getResourceAsStream("data/data.json");
+//        JsonReader reader = new JsonReader(new InputStreamReader(in));
+//        List<Category> allCategories = gson.fromJson(reader, new TypeToken<List<Category>>(){}.getType());
+//
+//        return allCategories;
     }
-
-    private List<Category> getMongoCategories()
-    {
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        MongoDatabase database = mongoClient.getDatabase("SmartFood");
-        //MongoCollection<Document> collection = database.getCollection("Categories");
-
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        MongoDatabase mongoDatabase = database.withCodecRegistry(pojoCodecRegistry);
-
-        MongoCollection<Category> collectionCategory = mongoDatabase.getCollection("Categories", Category.class);
-
-        List<Category> allCats = (List<Category>) collectionCategory.find().into(new ArrayList<Category>());
-
-        if(allCats.isEmpty())
-            return new ArrayList<>();
-        else
-            return allCats;
-    }
+//
+//    private List<Category> getMongoCategories()
+//    {
+//        MongoClient mongoClient = new MongoClient("localhost", 27017);
+//        MongoDatabase database = mongoClient.getDatabase("SmartFood");
+//        //MongoCollection<Document> collection = database.getCollection("Categories");
+//
+//        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+//                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+//        MongoDatabase mongoDatabase = database.withCodecRegistry(pojoCodecRegistry);
+//
+//        MongoCollection<Category> collectionCategory = mongoDatabase.getCollection("Categories", Category.class);
+//
+//        List<Category> allCats = (List<Category>) collectionCategory.find().into(new ArrayList<Category>());
+//
+//        if(allCats.isEmpty())
+//            return new ArrayList<>();
+//        else
+//            return allCats;
+//    }
 
 }
